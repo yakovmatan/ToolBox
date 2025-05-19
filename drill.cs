68231e01e408a1a55760e199d9d1b4e7e0fcc79f
yakov matan <yakovmatan@gmail.com>
2025-05-19 15:44:11 +0300

    build drill class

---
+++ drill.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Linq;
+using System;
+
+public class Drill : Tool
+{
+
+    public Drill(double weight):base("Drill",weight)
+    {
+
+    }
+
+    public override void Describe()
+    {
+        Console.WriteLine($"A {_name}, used for making holes. Weight: {_weight} kg.");
+    }
+
+
+    public override void Use()
+    {
+        Console.WriteLine("Drilling holes...");
+    }
+
+
+}
