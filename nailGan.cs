145289b933d615e12c159950b846ca2bdb66358c
yakov matan <yakovmatan@gmail.com>
2025-05-19 16:59:27 +0300

    create nailGun class

---
+++ nailGan.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Linq;
+using System;
+
+public class NailGun : Tool
+{
+
+    public NailGun(double weight):base("NailGun",weight)
+    {
+    }
+
+    public override void Describe()
+    {
+        Console.WriteLine($"A {_name}, fires nails quickly. Weight: {_weight} kg.");
+    }
+
+
+    public override void Use()
+    {
+        Console.WriteLine("Firing nails...");
+
+    }
+}
+
