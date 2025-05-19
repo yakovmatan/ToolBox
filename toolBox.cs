037b8651194c35284d948118c7014ee79f30a7ad
yakov matan <yakovmatan@gmail.com>
2025-05-19 12:39:45 +0300

    build class Tool

---
+++ toolBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Tool
+{
+
+    protected string _name;
+    protected int _weight;
+
+
+    public Tool(string Name,int Weight)
+    {
+        this._name = Name;
+        this._weight = Weight;
+    }
+
+    public void Describe()
+    {
+        Console.WriteLine($"name of the tool is: {_name}");
+        Console.WriteLine($"the weight of the tool is: {_weight}");
+    }
+
+    public void Use()
+    {
+        Console.WriteLine("Tool is being used");
+    }
+}
