4f9d2ceded0f15d107ea5962cf8edaf346d7aa05
yakov matan <yakovmatan@gmail.com>
2025-05-19 15:04:48 +0300

    create override to method from the tool

--- hammer.cs
+++ hammer.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+
+public class Hammer : Tool
+{
+    public Hammer(string Name, int Weight):base(Name, Weight)
+    {
+
+    }
+
+    public override void Describe()
+    {
+        Console.WriteLine($"A {_name}, great for pounding nails. weight: {_weight} kg.");
+    }
+
+    public override void Use()
+    {
+        Console.WriteLine("Hammering nails into wood...");
+    }
+}
